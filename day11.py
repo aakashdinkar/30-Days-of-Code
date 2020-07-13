6fb8df7cf69a2bae6aa279012cb589142e680d95
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-07-13 17:04:08 +0530

    Binary Numbers

--- day11.py
+++ day11.py
@@ -1,3 +1,34 @@
+'''
+Sample Input 1
+
+5
+
+
+Sample Output 1
+
+1
+
+Sample Input 2
+
+13
+
+
+Sample Output 2
+
+2
+
+
+Explanation
+
+Sample Case 1:
+The binary representation of  is , so the maximum number of consecutive 's is .
+
+Sample Case 2:
+The binary representation of  is , so the maximum number of consecutive 's is .
+
+'''
+
+
 #!/bin/python3
 
 import math
