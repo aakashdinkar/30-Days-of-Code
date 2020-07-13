708331d178dc152846176742dee0bd5d13c1074e
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-07-13 16:56:21 +0530

    Dictionaries and Maps

--- day9.py
+++ day9.py
@@ -1,3 +1,24 @@
+'''Sample Input
+
+3
+sam 99912222
+tom 11122222
+harry 12299933
+sam
+edward
+harry
+
+
+
+Sample Output
+
+sam=99912222
+Not found
+harry=12299933
+
+'''
+
+
 #!/bin/python3
 
 import math
