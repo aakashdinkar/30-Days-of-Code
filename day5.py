183646467dfd0d091b1ebed56a2d8712ac917d05
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-07-13 16:49:30 +0530

    Added sample input/output

--- day5.py
+++ day5.py
@@ -1,3 +1,28 @@
+'''Sample Input
+
+4
+-1
+10
+16
+18
+
+
+Sample Output
+
+Age is not valid, setting age to 0.
+You are young.
+You are young.
+
+You are young.
+You are a teenager.
+
+You are a teenager.
+You are old.
+
+You are old.
+You are old.'''
+
+
 class Person:
     def __init__(self,initialAge):
         self.initialAge = initialAge
