aeafd0e478ee76f3f7a86e46f5848fa8fea82798
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-07-14 19:29:42 +0530

    Data Types

--- day2.py
+++ day2.py
@@ -1,3 +1,18 @@
+'''
+Sample Input
+
+12
+4.0
+is the best place to learn and practice coding!
+
+Sample Output
+
+16
+8.0
+HackerRank is the best place to learn and practice coding!
+'''
+
+
 i = 4
 d = 4.0
 s = 'HackerRank '
