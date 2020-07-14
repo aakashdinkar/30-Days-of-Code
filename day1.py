909863aae74130fa40ef5f4934526b5e3fd1e9e6
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-07-14 19:29:18 +0530

    Hello, world.

--- day1.py
+++ day1.py
@@ -1,3 +1,13 @@
-val = input()
+'''Sample Input
 
-print(val+"!")
+Welcome to 30 Days of Code!
+
+Sample Output
+
+Hello, World. 
+Welcome to 30 Days of Code!'''
+
+
+val = input()
+print("Hello, world.")
+print(val + "!")
